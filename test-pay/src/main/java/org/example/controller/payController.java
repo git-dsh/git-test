1f1dce329a8c382db30eb7e00e1e1ed5f00888d9
段帅虎 <2803683394@qq.com>
2024-07-13 14:41:36 +0800

    5次提交

--- test-pay/src/main/java/org/example/controller/payController.java
+++ test-pay/src/main/java/org/example/controller/payController.java
@@ -53,7 +53,7 @@ public class payController {
     }
     @GetMapping("/updateOrderStatus")
     public void updateOrderStatus() throws Exception {
-        System.out.println("测试冲突1");
+        System.out.println("测试冲突1sdasdasd");
         String orderId = RandomUtil.randomNumbers(10);
         Boolean aBoolean = wxPayUtils.refreshWxPay(orderId);
         if(aBoolean){
